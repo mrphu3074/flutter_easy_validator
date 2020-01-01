f7611cfaf05b7bac19782fa6d42a01bb77d73e42
Phu.Nguyen <phu.nguyen@001-0324.local>
2020-01-01 14:35:28 +0700

    fix example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 ## [0.0.1] - TODO: Add release date.
 ## [1.0.0] - Stable version for object schema validation
+## [1.0.1] - Fix example
--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,40 @@
 import 'package:easy_validator/easy_validator.dart' as V;
 
 void main() {
-  final contactSchema = V.map().shape({
+  final schema = V.map().shape({
+    'email': V.string()
+      ..required()
+      ..email()
+      ..label = "Email",
+    'password': V.string()
+      ..required()
+      ..min(6)
+      ..max(8)
+      ..label = "Password",
+    'isRemember': V.bool()..label = "Remember"
+  });
+
+  // valid
+  Map errors;
+  errors =
+      schema.validate({'email': 'mrphu3074@gmail.com', 'password': '123456'});
+  print("VALID");
+  print(errors); // {} | Empty
+
+  // invalid
+  errors =
+      schema.validate({'email': 'mrphu3074@gmail', 'password': '123456789'});
+
+  print("INVALID");
+  print(errors);
+  // {
+  //    email: Email must be a valid email,
+  //    password: Password must be at most 8 characters
+  // }
+
+  // Advance schema
+  
+    final contactSchema = V.map().shape({
     'firstName': V.string(),
     'lastName': V.string(),
     'phone': V.string(),
